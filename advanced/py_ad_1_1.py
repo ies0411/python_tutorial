4535bde73286dc438b91b4e62bb517f2a28536c7
ies0411 <ies041196@gmail.com>
2024-01-10 17:01:05 +0900

    update

---
+++ advanced/py_ad_1_1.py
@@ -0,0 +1,97 @@
+"""
+chapter1
+python advanced - python variable scope
+scope, global, nonlocal, locals, globals
+"""
+
+
+a = 10  # global variable
+
+
+def foo():
+    # read global variable
+    print(a)  # -> 10
+
+
+foo()
+print(a)  # 10
+
+b = 20
+
+
+def bar():
+    b = 30
+    print(b)  # 30
+
+
+print(b)
+
+# ex3
+
+c = 40
+
+
+def foobar():
+    c = c + 10  # error
+    c = 10
+    c += 100
+
+    print(c)
+
+
+foobar()
+
+
+# ex4
+d = 50
+
+
+def barfoo():
+    global d  # global 예약어를 통해 수정가능 -> but global 선언은 자제하자
+    d = d + 10
+    d += 100
+    print(d)
+
+
+barfoo()
+
+
+# ex5
+# closure
+def outer():
+    e = 70
+
+    def inner():
+        nonlocal e  # important!
+        e += 10
+        print(e)
+
+    return inner
+
+
+in_test = outer()  # closure
+in_test()
+
+
+def func(var):
+    x = 10
+
+    def printer():
+        print("inner")
+
+    print("fucn", locals())
+
+
+func("Hi")
+
+
+print("ex7", globals())
+test_variable = 100
+# globals()["test_variable"]=100
+print(globals())
+
+for i in range(1, 10):
+    for k in range(1, 10):
+        globals()["plus_{}_{}".format(i, k)] = i + k
+print(globals())
+print(plus_5_5)
