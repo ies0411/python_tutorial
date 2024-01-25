9100fa6527f811278d354abb6c8655a2e82f353a
ies0411 <ies041196@gmail.com>
2024-01-25 14:40:17 +0900

    update

---
+++ advanced/3/py_ad_3_1.py
@@ -0,0 +1,23 @@
+"""
+    meta:class of class, type, meta class, custom meta class
+    1. class만드는 역할 -> class custom
+    2. framework작성시 필수
+    3. 동적 생성(type), 커스텀 생성(type)함수
+    4. 커스텀 클래스 -> 검증클래스 등
+    5. 엄격한 class 사용 요구, 메소드 오버라이드 요구
+"""
+
+
+# type 예제
+class SampleA:  # class == object, != instance
+    pass
+
+
+obj1 = SampleA()
+# obj1 == instance
+obj2 = SampleA()
+
+print(obj1.__class__)
+print(type(obj1))
+print(obj1.__class__ is type(obj1))  # true
+print(obj1.__class__.__class__)  # type
